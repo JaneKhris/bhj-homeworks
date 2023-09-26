ed79df086bf0c49f0ca93116d43c4dd6d5d1c813
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2023-09-27 00:27:10 +0300

    correct menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,21 @@
-const array_menu_sub = Array.from(document.querySelectorAll('.menu_sub'))
-array_menu_sub.forEach((element) => {
+const arrayMenuSub = Array.from(document.querySelectorAll('.menu_sub'))
+arrayMenuSub.forEach((element) => {
     parent = element.closest('.menu__item')
     link = parent.querySelector('.menu__link')
     link.onclick = () => {
-        element.className = 'menu menu_sub menu_active'
-        parent_menu = element.closest('.menu_main')
-        array_menu = parent_menu.querySelectorAll('.menu_sub')
-        array_menu.forEach((el) => {
-            if (el != element) {
-                el.className = 'menu menu_sub'
-            }
-        })
+        if (element.className.includes('menu_active')) {
+            element.className = 'menu menu_sub'
+        }
+        else {
+            element.className = 'menu menu_sub menu_active'
+            parentMenu = element.closest('.menu_main')
+            arrayMenu = parentMenu.querySelectorAll('.menu_sub')
+            arrayMenu.forEach((el) => {
+                if (el != element) {
+                    el.className = 'menu menu_sub'
+                }
+            }) 
+        }   
         return false
     }
 })
