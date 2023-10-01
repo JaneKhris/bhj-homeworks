acf0d00b0abeda7344e461c8f6f8562386ef031a
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2023-10-01 19:55:31 +0300

    task2

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,15 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    this.currentSymbol = document.querySelector('.symbol_current')
+    document.addEventListener('keyup',(event) => {
+      if (this.currentSymbol.textContent.toLowerCase() == event.key.toLowerCase()) {
+        this.success()
+      }
+      else {
+        this.fail()
+      }
+    })
   }
 
   success() {
