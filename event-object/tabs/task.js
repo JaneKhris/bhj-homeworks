429f93d03cd78a2f737ade456bbd524f54a78f1d
JaneKhris <efkhrisanfova@gmail.com>
2023-10-31 13:36:22 +0300

    tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,7 +3,10 @@ const contents = Array.from(document.querySelectorAll('.tab__content'))
 
 tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
-        
+        tab.closest('.tabs').querySelector('.tab_active').className = "tab";
+        tab.className = "tab tab_active";
+        tab.closest('.tabs').querySelector('.tab__content_active').className = "tab__content"
+        contents[tabs.indexOf(tab)].className = "tab__content tab__content_active"
     })
 
 })
