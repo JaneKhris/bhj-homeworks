fbe99a74dafbee0fbfa37c2053d12bb5fae6d3bf
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2023-10-31 11:40:06 +0300

    task3

---
+++ event-object/tabs/task.js
@@ -0,0 +1,10 @@
+const tabs = Array.from(document.querySelectorAll('.tab'))
+const contents = Array.from(document.querySelectorAll('.tab__content'))
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        
+    })
+
+})
+console.log(tabs)
