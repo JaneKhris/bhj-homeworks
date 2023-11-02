43b91ace805d75eefc8ced022bff9c0f70d52cb5
JaneKhris <efkhrisanfova@gmail.com>
2023-11-02 16:20:48 +0300

    correct

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -7,12 +7,12 @@ setInterval(() => {
     el.classList.remove('rotator__case_active');
     if (i<array.length-1) {
         let next = el.nextElementSibling;
-        elemActivate(next)
+        elemActivate(next);
         i += 1;
     }
     else {
         let first = array[0];
-        elemActivate(first)
+        elemActivate(first);
         i = 0;
     }
 },1000)
