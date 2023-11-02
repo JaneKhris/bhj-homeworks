6432f06507ffd5057a216757888235d77c203a2f
JaneKhris <efkhrisanfova@gmail.com>
2023-11-02 13:07:40 +0300

    ads correct

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,27 +1,28 @@
-const array = Array.from(document.querySelector('.rotator').children)
-let i = 0
+const array = Array.from(document.querySelector('.rotator').children);
+let i = 0;
+
+
 setInterval(() => {
     let el= array[i];
     el.classList.remove('rotator__case_active');
     if (i<array.length-1) {
-        el.nextElementSibling.classList.add('rotator__case_active');
-        el.nextElementSibling.setAttribute("style", getColorString());
+        let next = el.nextElementSibling;
+        elemActivate(next)
         i += 1;
     }
     else {
-        array[0].classList.add('rotator__case_active');
-        array[0].setAttribute("style", getColorString());
-        i = 0
+        let first = array[0];
+        elemActivate(first)
+        i = 0;
     }
 },1000)
 
-function getColorString() {
-    const words = [
-        'Red',
-        'Blue',
-        'Green',
-        'Orange',
-      ],
-      index = Math.floor(Math.random() * words.length);
-    return `color:${words[index]}`;
+function getColor(elem) {
+    return `color:${elem.dataset.color}`;
 }
+
+ function elemActivate(elem) {
+    elem.classList.add('rotator__case_active');
+    elem.setAttribute("style", getColor(elem));
+ }
+
