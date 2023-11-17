c597b5d92ffdf0175a0a7a26e7067f9482273181
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2023-11-17 23:42:14 +0300

    todo base

---
+++ document-structure/todo/task.js
@@ -0,0 +1,24 @@
+const button = document.getElementById('tasks__add');
+const input = document.getElementById('task__input');
+const taskList = document.getElementById('tasks__list');
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (input.value) {
+        let div = document.createElement('div');
+        div.innerHTML = `
+        <div class="task">
+            <div class="task__title">${input.value}</div>
+            <a href="#" class="task__remove">&times;</a>
+        </div>
+        `
+        input.value = '';
+        console.log(div);
+        taskList.appendChild(div);
+        let taskRemove = div.querySelector('.task__remove');
+        taskRemove.addEventListener('click', (event) => {
+            event.preventDefault();
+            taskRemove.closest('.task').remove();
+        })
+    }
+})
