1e39c86413ba68e8c5bbbcfd2fc47945000ce38f
JaneKhris <efkhrisanfova@gmail.com>
2023-11-28 16:42:19 +0300

    progressbar correct

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -5,23 +5,10 @@ document.forms[0].addEventListener('submit', (e) => {
     e.preventDefault();
 
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange',() => {
-        if (xhr.readyState == 0) {
-            progress.value = 0;
-        }
-        else if (xhr.readyState == 1) {
-            progress.value = 0.25;
-        }
-        else if (xhr.readyState == 2) {
-            progress.value = 0.5;
-        }
-        else if (xhr.readyState == 3) {
-            progress.value = 0.75;
-        }
-        else if (xhr.readyState == 4) {
-            progress.value = 1;
-        }
-    })
+
+    xhr.upload.onprogress = function(event) {
+        progress.value = event.loaded/event.total;
+      };
     
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
 
