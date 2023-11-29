1cf7d84b17e4e8182a071979e966aa4c6b977fec
JaneKhris <efkhrisanfova@gmail.com>
2023-11-29 14:22:08 +0300

    texteditor additional

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,19 @@
+const storedText = localStorage.getItem('text');
+const editor = document.getElementById('editor');
+
+if(storedText) {
+    editor.value = storedText;
+} 
+setInterval(() => {
+    localStorage.setItem('text', editor.value );
+},500)
+
+const button = document.createElement('button');
+button.className = 'button_clear';
+button.innerHTML = 'Очистить поле';
+document.querySelector('main').appendChild(button);
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    editor.value = '';
+})
+
