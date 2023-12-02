eb5b522ae14ac290861850723116fa4e44679c22
JaneKhris <efkhrisanfova@gmail.com>
2023-12-02 15:54:53 +0300

    popup correct

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -2,15 +2,19 @@ const modal = document.querySelector('.modal');
 const modalClose = document.querySelector('.modal__close');
 
 
-if (document.cookie) {
-    modal.className = 'modal';
-} else {
+
+if (!getCookie('modal')) {
     modal.className = ' modal modal_active';
-    document.cookie = "modal=close";
 }
 
 modalClose.addEventListener('click', (e) => {
     modal.classList.remove('modal_active');
-    // document.cookie = 'modal=close';
-    // console.log(document.cookie)
+    document.cookie = 'modal=close';
 })
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
